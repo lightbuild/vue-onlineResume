da6ab9378a4694773f999d747a02333dc8bbed87
Light Build <893458528@qq.com>
2017-01-16 17:37:07 +0800

    step01-webpack

---
+++ step01-webpack/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2017/1/15.
+ */
+import bar from './app.js';
+bar();
---
+++ step01-webpack/bar.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/1/15.
+ */
+export default function bar() {
+    alert("Hello Webpack");
+}
