19427424e03ecfe3c388373dcc66a3b22aaf485f
Light Build <893458528@qq.com>
2017-01-16 17:47:08 +0800

    vue-test

---
+++ step02-vue/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/1/15.
+ */
+import bar from './bar';
+import Vue from 'vue';
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+bar();
