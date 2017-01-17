d3bceedec0182d3bca539b06c9d05ad6bcbb4f16
Light Build <893458528@qq.com>
2017-01-17 23:11:19 +0800

    Todo

---
+++ step03-Todo/webpack.config.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2017/1/15.
+ */
+var Webpack = require("webpack");
+module.exports = {
+    entry: './app.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module:{
+        loaders:[
+            {
+                test: /\.js[x]?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader?presets[]=es2015&presets[]=react'
+            },
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        }
+    }
+}
